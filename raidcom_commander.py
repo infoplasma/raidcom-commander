8f1eba0d70a4f90879dfd47fccd72199baf57d8c
infoplasma <lorenzoamante@infoplasma.com>
2019-12-10 00:53:59 +0100

    Initial commit.

---
+++ raidcom_commander.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python3
+
+print("*** INFO: LOADING PROGRAM, PLEASE BE PATIENT. ***")
+ 
+import npyscreen as nps
+from ldev_creator import ldev_creatorForm
+from ldev_terminator import ldev_terminatorForm
+
+
+class myEntryForm(nps.FormBaseNewWithMenus):
+    def create(self):
+        self.pager = self.add(nps.Pager, values=["WELCOME TO RAIDCOMMANDER", "PRESS CTRL-X TO START. THANK YOU."])
+        self.menu = self.new_menu(name='OPTION MENU', shortcut=None)
+        self.menu.addItem(text='LDEV PROVISIONER..............', onSelect=self.ldev_creator, shortcut='1')
+        self.menu.addItem(text="LDEV TERMINATOR...............", onSelect=self.ldev_terminator, shortcut='2')
+        self.menu.addItem(text='CONFIGURATOR PARAMETRI........', onSelect=self.configurator_parametri, shortcut='3')
+        self.menu.addItem(text='EXODUS........................', onSelect=self.exit_func, shortcut='0')
+
+    def configurator_parametri(self):
+        self.parentApp.switchForm("CONFIGURATOR PARAMETRI")
+
+    def ldev_creator(self):
+        self.parentApp.switchForm("LDEV PROVISIONER")
+
+    def ldev_terminator(self):
+        self.parentApp.switchForm("LDEV TERMINATOR")
+
+    def exit_func(self):
+        nps.notify_wait("*** INFO: EXITING PROGRAM: >>> GOODBYE! <<< ***")
+        self.parentApp.switchForm(None)
+
+
+class configuratorForm(nps.ActionForm):
+    def activate(self):
+        nps.notify_wait("to change configuration settings you must manually edit config/config.yaml", title='WARNING')
+        self.parentApp.setNextForm("MAIN")
+    
+    def create(self):
+        pass
+
+    def on_ok(self):
+        self.parentApp.setNextForm("MAIN")
+
+
+class MyApplication(nps.NPSAppManaged):
+    def onStart(self):
+        nps.setTheme(nps.Themes.ElegantTheme)
+        self.addForm('MAIN', myEntryForm, name='RAIDCOMMANDER')
+        self.addForm('LDEV PROVISIONER', ldev_creatorForm, name="LDEV PROVISIONER")
+        self.addForm('LDEV TERMINATOR', ldev_terminatorForm, name="LDEV TERMINATOR")
+        self.addForm("CONFIGURATOR PARAMETRI", configuratorForm, name="CONFIGURATOR PARAMETRI")
+        
+
+if __name__ == "__main__":
+    MyApplication().run()
+    print('*** INFO: PROGRAM ENDED, GOODBYE. ***')
---
+++ writers.py
@@ -0,0 +1,54 @@
+from jinja2 import Environment, FileSystemLoader
+from yaml import safe_dump, safe_load
+
+
+with open('config/config.yaml') as yaml_data:
+    cfg = safe_load(yaml_data)
+
+
+def write_yaml(ldev_prefix='none',
+               ldevs_gb='none',
+               is_gad='none',
+               gad_res_name='none',
+               gad_dev_grp='0',
+               pool_id='none',
+               ser_pri='none',
+               ser_sec='none',
+               ldevs='none'):
+    ldevs = [i.replace(":", "") for i in ldevs]
+    ldev_dict = dict(zip(ldevs, ldevs_gb))
+
+    data = {'ldevs': [{"ldev_id": i, "ldev_gb": j} for i, j in ldev_dict.items()],
+            'ldevs_pfix': ldev_prefix,
+            'gad':
+                {'gad_grp': gad_dev_grp,
+                 'gad_res_name': gad_res_name,
+                 'is_gad': is_gad},
+            'plural': '' if len(ldev_dict) == 1 else 's',
+            'pool': {'pool_id': pool_id},
+            'ser_pri': ser_pri,
+            'ser_sec': ser_sec}
+
+
+    with open("vars/output_params.yaml", "w", encoding='utf-8') as handle:
+        safe_dump(data, handle)
+
+
+def write_output(action_type):
+    """
+    """
+
+    # Read the configuration file
+    with open("vars/output_params.yaml", "r") as handle:
+        devs = safe_load(handle)
+
+    j2_env = Environment(loader=FileSystemLoader("."), trim_blocks=True, autoescape=True)
+
+    if action_type == 'terminate':
+        template = j2_env.get_template("templos/{}_terminator_templo.j2".format("gad" if devs['gad']['is_gad'] else "nogad"))
+    else:
+        template = j2_env.get_template("templos/{}_templo.j2".format("gad" if devs['gad']['is_gad'] else "nogad"))
+
+    config = template.render(data=devs)
+    with open(cfg['DEFAULT_OUTPUT_FILE'], "w") as output:
+        output.write(config)
